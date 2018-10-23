228370a995d4c2d9113ac06b02bf07bf0cd1966b
Bean <bcb1486@gmail.com>
2018-10-23 19:09:43 -0400

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -3,7 +3,7 @@ from credentials import account_sid, auth_token, my_cell, my_twilio
 
 client = TwilioRestClient(account_sid, auth_token)
 
-my_msg = ''.join(['silly bob\n' for i in range(100)])
+my_msg = ''.join(['HelloOOO!!!\n' for i in range(100)])
 
 message = client.messages.create(to=my_cell, from_=my_twilio,
 						body=my_msg)
