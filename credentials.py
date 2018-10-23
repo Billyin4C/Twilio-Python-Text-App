1f4ef9e1d8ff9def6ba1c4b67e1b36fff34c9e13
Bean <bcb1486@gmail.com>
2018-10-23 19:07:56 -0400

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
 account_sid = 'AC97928ff6a70f2a1d791c58e110171294'
 auth_token = 'ebb388aea7fedf50ae7612887744d210'
-my_cell = '+17048064744'
+my_cell = '+1(Your cell # here)'
 my_twilio = '+18036755441'
